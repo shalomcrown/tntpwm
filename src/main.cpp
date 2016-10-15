79a17537bc7b0d1ae4c98b9db3777a45182f7684
Shalom Crown <shalomcrown@gmail.com>
2016-10-15 22:03:14 +0300

    Logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <tnt/tntnet.h>
+#include <cxxtools/log.h>
+
+
+log_define("accumulator")
 
 int main(int argc, char **argv) {
     try  {
+
+        log_init();
+
+
         tnt::Tntnet app;
         app.listen(8080);
         app.mapUrl("^/$", "mainpage");
         app.mapUrl("^/([^.]+)(\\..+)?", "$1");
+
+        log_info("Starting up");
         app.run();
         return 0;
 
